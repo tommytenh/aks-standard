5dbf01a7532b2f2a5c2f07c111c01669bf42f0c1
Tuomas Tenhunen <tuomas.tenhunen@outlook.com>
2023-11-20 19:59:41 +0000

    removed subnet ids

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -13,6 +13,4 @@ module "vnet" {
   address_space       = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.0.0/24", "10.0.1.0/24"]
   subnet_names        = ["aks-subnet", "app-gateway-subnet"]
-  subnet_ids          = []
-  dns_servers         = []
 }
