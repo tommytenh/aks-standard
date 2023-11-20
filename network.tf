830c74a0279cae2fdd1612282cdbb18e26271647
Tuomas Tenhunen <tuomas.tenhunen@outlook.com>
2023-11-20 17:32:56 +0000

    commit

--- network.tf
+++ network.tf
@@ -4,7 +4,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "vnet" {
-  source  = "registry.terraform.ioAzure/vnet/azurerm"
+    source  = "registry.terraform.ioAzure/vnet/azurerm"
+    version = "4.1.0"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
     address_space       = ["10.0.0.0/24"]
