7a59794f7a52a465f7b2dc5f27f13433d1a07eeb
Tuomas Tenhunen <tuomas.tenhunen@outlook.com>
2023-11-20 17:28:54 +0000

    added the path

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "vnet" {
-  source  = "Azure/vnet/azurerm"
+  source  = "registry.terraform.ioAzure/vnet/azurerm"
   version = "4.1.0"
   
     resource_group_name = azurerm_resource_group.rg.name
